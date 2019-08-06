49652a2c10eb83dc876e359e7d5e1c8e27a4ccdf
Barbara Whiting <whitingba@gmail.com>
2019-08-06 17:18:46 -0600

    changes to moment.js, trying to get the correct time as input into form

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,9 +22,12 @@ $(document).ready(function () {
         //grab the value from the form field and trim and whitespace
         let trainName = $('#trainNameInput').val().trim();
         let destinationName = $('#destinationInput').val().trim();
-        let trainTime = moment($('#trainTimeInput').val().trim(), "HH:mm").format("X");
+        let trainTime = $('#trainTimeInput').val().trim();  //, "HH:mm").format();
         let frequencyTime = $('#frequencyInput').val().trim();
 
+
+
+        //console.log(translateTime);
 
         //create local 'temp' object for holding train data and push data to firebase
         database.ref().push({
@@ -49,43 +52,49 @@ $(document).ready(function () {
 
 
 
-//TODO: firebase event to add train to my html from firebase database
+//firebase event to add train to my html from firebase database
 database.ref().on('child_added', function (childSnapshot) {
-    console.log(childSnapshot.val());
+    // console.log(childSnapshot.val());
 
     //store the data into a variable
     let trainName = childSnapshot.val().name;
+    console.log(trainName);
     let destinationName = childSnapshot.val().destination;
+    console.log(destinationName);
     let trainTime = childSnapshot.val().time;
+    console.log("CURRENT TIME: " + moment(trainTime).format("hh:mm"));
     let frequencyTime = childSnapshot.val().frequency;
+    console.log(frequencyTime);
+
+    let translateTime = moment(trainTime, "HH:mm");
 
     //put the time the first train leaves into a time that is readable
-    let translateTime = moment.unix(trainTime).format("HH:mm");
+    // let translateTime = moment(trainTime).format("HH:mm");
+    // console.log(translateTime);//FIXME: putting current time, not time that was input
+
+    //TODO:
+    //User inputs the time the 1st train leaves as well as how often the train comes (frequency)
+    ///Need to use the inputs above to calculate the next arrival time using the current time and the frequency of the train as well as time the first train comes.
+    ////As well as use the difference of next arrival time less the last arrival time to calculate how many minutes away the next train is
+    /////some of this could be simple math and some will need moment.js
 
 
-
-
-
-
-    //create new rows
+    //FIXME:
+    //create new rows and have data from firebase append to them
     let newRow = $("<tr>").append(
         $("<td>").text(trainName),
         $("<td>").text(destinationName),
         $("<td>").text(frequencyTime),
         $("<td>").text(translateTime),
-        // $("<td>").text()
-
+        //TODO: $("<td>").text() - will need one for next arrival
+        //TODO: $("<td>").text() - will need one for minutes away
     );
 
 
 
 });
 
-//TODO:
-//User inputs the time the 1st train leaves as well as how often the train comes (frequency)
-///Need to use the inputs above to calculate the next arrival time using the current time and the frequency of the train as well as time the first train comes.
-////As well as use the difference of next arrival time less the last arrival time to calculate how many minutes away the next train is
-/////some of this could be simple math and some will need moment.js
+
 
 
 
